0c6369e0d52ef63220d5f27bd9d0f7731961b9d7
niklas.kahlhoff <L!ber!a20-02>
2023-05-11 16:22:10 +0200

    Adjust Teleporter

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class Teleporter : MonoBehaviour
 {
     [SerializeField]
-    private int sceneInx;
+    private string sceneInx;
     // Start is called before the first frame update
     void Start()
     {
